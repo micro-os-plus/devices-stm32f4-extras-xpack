678a117bfea149477ff89dc4a3dc86e00db74f48
Liviu Ionescu <ilg@livius.net>
2021-01-14 00:11:04 +0200

    clang-format

--- include/micro-os-plus/device.h
+++ include/micro-os-plus/device.h
@@ -28,8 +28,8 @@
 #ifndef MICRO_OS_PLUS_DEVICE_H_
 #define MICRO_OS_PLUS_DEVICE_H_
 
-#include <micro-os-plus/architecture.h>
 #include "stm32f4xx.h"
+#include <micro-os-plus/architecture.h>
 
 #endif // MICRO_OS_PLUS_DEVICE_H_
 
