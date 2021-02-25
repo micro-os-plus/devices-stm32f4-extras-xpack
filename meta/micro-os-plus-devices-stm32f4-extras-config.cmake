b258960869e89fc23a91354f7206526ef8f3be3c
Liviu Ionescu <ilg@livius.net>
2021-02-25 16:43:49 +0200

    cmake: simplify, keep only config.cmake

--- meta/micro-os-plus-devices-stm32f4-extras-config.cmake
+++ meta/micro-os-plus-devices-stm32f4-extras-config.cmake
@@ -12,11 +12,79 @@
 # https://cmake.org/cmake/help/v3.18/
 # https://cmake.org/cmake/help/v3.18/manual/cmake-packages.7.html#package-configuration-file
 
-# message(STATUS "Including micro-os-plus-devices-stm32f4-extras-config...")
+if(micro-os-plus-devices-stm32f4-extras-included)
+  return()
+endif()
+
+set(micro-os-plus-devices-stm32f4-extras-included TRUE)
+
+message(STATUS "Including micro-os-plus-devices-stm32f4-extras...")
+
+# -----------------------------------------------------------------------------
+# Preprocessor symbols.
+
+# TODO: migrate to options.
+set(xpack_device_family_compile_definition "STM32F4")
+message(STATUS "${xpack_device_family_compile_definition}")
+
+# This also requires xpack_device_compile_definition to be defined by the user.
+
+# -----------------------------------------------------------------------------
+# Dependencies.
+
+find_package(micro-os-plus-architecture-cortexm REQUIRED)
+
+# ---------------------------------------------------------------------------
+# The current folder.
+
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
 # -----------------------------------------------------------------------------
 
-include("${CMAKE_CURRENT_LIST_DIR}/xpack-helper.cmake")
+if(NOT TARGET micro-os-plus-devices-stm32f4-extras-interface)
+
+  add_library(micro-os-plus-devices-stm32f4-extras-interface INTERFACE EXCLUDE_FROM_ALL)
+
+  # Hopefully the file names follow the symbol definitions.
+  string(TOLOWER ${xpack_device_compile_definition} device_name)
+  
+  # ---------------------------------------------------------------------------
+
+  target_sources(
+    micro-os-plus-devices-stm32f4-extras-interface
+
+    INTERFACE
+      ${xpack_current_folder}/src/vectors/vectors_${device_name}.c
+  )
+
+  target_include_directories(
+    micro-os-plus-devices-stm32f4-extras-interface
+
+    INTERFACE
+      ${xpack_current_folder}/include
+  )
+
+  target_compile_definitions(
+    micro-os-plus-devices-stm32f4-extras-interface
+
+    INTERFACE
+      "${xpack_device_family_compile_definition}"
+      "${xpack_device_compile_definition}"
+  )
+
+  target_link_libraries(
+    micro-os-plus-devices-stm32f4-extras-interface
+
+    INTERFACE
+      micro-os-plus::architecture-cortexm
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(micro-os-plus::devices-stm32f4-extras ALIAS micro-os-plus-devices-stm32f4-extras-interface)
+  message(STATUS "micro-os-plus::devices-stm32f4-extras")
+
+endif()
 
 # -----------------------------------------------------------------------------
-
