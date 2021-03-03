e0bf3f2a71bea24213728300a8deca7941e7239f
Liviu Ionescu <ilg@livius.net>
2021-03-04 00:32:16 +0200

    cmake: add xpack_process_package_version()

--- meta/micro-os-plus-devices-stm32f4-extras-config.cmake
+++ meta/micro-os-plus-devices-stm32f4-extras-config.cmake
@@ -18,7 +18,7 @@ endif()
 
 set(micro-os-plus-devices-stm32f4-extras-included TRUE)
 
-message(STATUS "Including micro-os-plus-devices-stm32f4-extras...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # Preprocessor symbols.
