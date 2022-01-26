e4fc10548bcee13b8115cd887420f1e6e1c94df7
Liviu Ionescu <ilg@livius.net>
2022-01-26 09:07:39 +0200

    rework CMake

---
+++ cmake/add_devices_libraries.cmake
@@ -0,0 +1,37 @@
+#
+# This file is part of the µOS++ distribution.
+#   (https://github.com/micro-os-plus)
+# Copyright (c) 2021 Liviu Ionescu
+#
+# This Source Code Form is subject to the terms of the MIT License.
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/MIT/.
+#
+# -----------------------------------------------------------------------------
+
+# TODO: generate it automatically.
+add_micro_os_plus_device_stm32f4_extra_library("STM32F401xc")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F401xe")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F405xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F407xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F410cx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F410rx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F410tx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F411xe")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F412cx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F412rx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F412vx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F412zx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F413xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F415xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F417xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F423xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F427xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F429xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F437xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F439xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F446xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F469xx")
+add_micro_os_plus_device_stm32f4_extra_library("STM32F479xx")
+
+# -----------------------------------------------------------------------------
